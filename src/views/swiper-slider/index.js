b1dd44e0e04eef2e29ecacb4fa9ca82fb5e13947
winsycwen <coding@winsycwen>
2018-04-09 16:36:02 +0800

    Update: renamed some files and modified page logic

--- src/views/swiper-slider/index.js
+++ src/views/swiper-slider/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world')
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './index.vue'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
+})
